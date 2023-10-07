836ff3d339ae3ffe6f4cca8be3d824683299c8a0
Daudi Mwanzia <daudimwanzia@gmail.com>
2023-10-07 19:45:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail
+    }),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((data) => {
+      const newId = data.id;
+      const newElement = document.createElement("p");
+      newElement.textContent = newId;
+      document.body.appendChild(newElement);
+      return data;
+    })
+    .catch((error) => {
+      const errorMessage = document.createElement("p");
+      errorMessage.textContent = error.message;
+      document.body.appendChild(errorMessage);
+    });
+}
